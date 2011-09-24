5e25c7da14bb531cfd95adee4eb66b58ff94abb1
Jan E <jeebjp@gmail.com>
2011-09-24 20:48:37 +0300

    (CCCP Patch) CCCP LAV Filters' defaults.
    Enable the mixer for now, as Lord had problems with
    the Windows one. Maybe with NT6 we'll all be able to
    disable this without thinking too much about it :V
    
    Also enable the LAV Splitter's tray icon for testing for now.

--- demuxer/LAVSplitter/LAVSplitter.cpp
+++ demuxer/LAVSplitter/LAVSplitter.cpp
@@ -150,16 +150,19 @@ STDMETHODIMP CLAVSplitter::JoinFilterGraph(IFilterGraph * pGraph, LPCWSTR pName)
 static BOOL get_iformat_default(std::string name)
 {
   // Raw video formats lack timestamps..
-  if (name == "rawvideo") {
-    return FALSE;
+  if (name == "flac" || name == "flv" || name == "mp4" || name == "mpegts" || name == "amr" ||
+      name == "ogg" || name == "rm" || name == "rtp" || name == "rtsp" || name == "matroska" ||
+      name == "mxf" || name == "bink" || name == "smk" || name == "pmp" || name == "nut" ||
+      name == "hls,applehttp" || name == "flic" || name == "avisynth") {
+    return TRUE;
   }
 
-  return TRUE;
+  return FALSE;
 }
 
 STDMETHODIMP CLAVSplitter::LoadDefaults()
 {
-  m_settings.TrayIcon         = FALSE;
+  m_settings.TrayIcon         = TRUE;
 
   m_settings.prefAudioLangs   = L"";
   m_settings.prefSubLangs     = L"";
