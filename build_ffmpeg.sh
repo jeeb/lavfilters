cc42e88a67ed41d7690a00d7dbd17a6465efa0f8
Jan Ekström <jeebjp@gmail.com>
2013-03-20 19:21:32 +0200

    (CCCP Patch) Enable some additional things in ffmpeg
    Also contains extra stuff to make both build scripts work locally

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -60,6 +60,7 @@ configure() (
     --enable-runtime-cpudetect \
     --enable-demuxers \
     --disable-demuxer=matroska \
+    --enable-avisynth \
     --disable-filters \
     --enable-filter=yadif \
     --enable-filter=scale \
@@ -70,6 +71,8 @@ configure() (
     --enable-protocol=mmst \
     --enable-protocol=rtp \
     --enable-protocol=http \
+    --enable-protocol=rtmp \
+    --enable-protocol=rtmpt \
     --disable-muxers \
     --enable-muxer=spdif \
     --enable-dxva2 \
@@ -114,7 +117,7 @@ configure() (
 )
 
 build() (
-  make -j8
+  make -j2
 )
 
 echo Building ffmpeg in GCC ${arch} Release config...
