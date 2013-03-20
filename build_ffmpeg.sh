f83b260aa1ca91c05c7f0152b91753d58bd1e847
Jan Ekström <jeebjp@gmail.com>
2013-03-20 19:21:32 +0200

    (CCCP Patch) Enable some additional things in ffmpeg
    Also contains extra stuff to make both build scripts work locally

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -71,6 +71,8 @@ configure() (
     --enable-protocol=rtp           \
     --enable-protocol=http          \
     --enable-protocol=crypto        \
+    --enable-protocol=rtmp          \
+    --enable-protocol=rtmpt         \
     --disable-muxers                \
     --enable-muxer=spdif            \
     --disable-hwaccels              \
@@ -112,7 +114,7 @@ configure() (
 )
 
 build() (
-  make -j8
+  make -j4
 )
 
 echo Building ffmpeg in GCC ${arch} Release config...
