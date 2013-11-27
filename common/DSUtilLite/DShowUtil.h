fa3ec7478287722180e34cff3272fad508b065b3
Lord <lord@rizon>
2013-11-27 10:59:49 +0200

    (CCCP Patch) Enable colorimetry with VSFilter and overlay
    VSFilter either ignores or uses (and doesn't pass on) colorimetry
    with overlay.

--- common/DSUtilLite/DShowUtil.h
+++ common/DSUtilLite/DShowUtil.h
@@ -113,6 +113,7 @@ extern HRESULT FindPinIntefaceInGraph(IPin *pPin, REFIID refiid, void **pUnknown
 extern HRESULT FindFilterSafe(IPin *pPin, const GUID &guid, IBaseFilter **ppFilter, BOOL bReverse = FALSE);
 extern BOOL FilterInGraphSafe(IPin *pPin, const GUID &guid, BOOL bReverse = FALSE);
 extern BOOL HasSourceWithType(IPin *pPin, const GUID &mediaType);
+extern HRESULT getConnectedFilterCLSIDSafe(IPin *pPin, GUID &clsid);
 
 std::wstring WStringFromGUID(const GUID& guid);
 BSTR ConvertCharToBSTR(const char *sz);
